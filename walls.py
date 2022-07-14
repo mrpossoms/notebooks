07fb115310df877c4d6e56b3dbf23129482d685b
Kirk Roerig <mr.possoms@gmail.com>
2022-07-14 14:59:19 -0600

    Wall panel layout generation

--- walls.py
+++ walls.py
@@ -2,28 +2,80 @@
 
 import cv2
 import numpy as np
+import random
 
-r, c = 12 * 10, 12 * 10
+r, c = 90, 122
 wall = np.zeros((r, c, 3))
-mask = [[None] * c for _ in range(r)]
+mask = np.zeros((r, c, 3))
 
 print(mask)
 
 def pieces():
-	return [
+	pieces = [
 		(24, 16, 1),
 		(24, 32, 1),
 		(12, 16, 1),
 		(24, 16, 2),
 		(24, 32, 2),
 		(12, 16, 2),
+		(48, 32, 2),
+		(32, 48, 2),
+		(48, 48, 2),
 	]
+	random.shuffle(pieces)
+	return pieces
+
+def score(r, c, piece):
+	area = piece[0] * piece[1]
+	scr = 0#area
+	other_piece = mask[r][c]
+
+	if other_piece is None:
+		return src + 2
+
+	if (other_piece == piece).all():
+		scr -= 1
+
+	if (other_piece == rotate_piece(piece)).all():
+		scr += 1
+
+	if other_piece[2] != piece[2]:
+		scr += 1
+
+	scr -= mask[r:r+piece[0], c:c+piece[1]].sum()
+
+	return scr #np.random.randint(0, 5)
+
+def select_piece(r, c):
+	scores = {p:0 for p in pieces()}
+
+	for ri in range(-1, 2):
+		for ci in range(-1,2):
+			for p in pieces():
+				scores[p] += score(r, c, p)
+
+	best = list(scores.keys())[0]
+
+	for p in pieces():
+		if scores[best] < scores[p]:
+			best = p
+
+	return best
 
 def rotate_piece(piece):
 	return (piece[1], piece[0], piece[2])
 
 def set_piece(r, c, piece):
-	wall[r:piece[0], c:piece[1]] = piece[2]
-	mask[r][c] = piece
+	wall[r:r+piece[0], c:c+piece[1]] = 0
+	wall[r+1:r+piece[0]-1, c+1:c+piece[1]-1] = [piece[2] * 32] * 3
+	mask[r:r+piece[0], c:c+piece[1]] = piece
+
+for r in range(wall.shape[0]):
+	for c in range(wall.shape[1]):
+		# print(mask[r,c])
+		if mask[r,c][0] != 0 and mask[r,c][1] != 0:
+			continue
+
+		set_piece(r, c, select_piece(r, c))
 
 cv2.imwrite("walls.png", wall)
