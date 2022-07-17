39ad2508da65c749491820d02b894a519b5af974
Kirk Roerig <mr.possoms@gmail.com>
2022-07-16 23:03:19 -0600

    Stuff

--- walls.py
+++ walls.py
@@ -3,20 +3,10 @@
 import cv2
 import numpy as np
 import random
+import collections
+import shutil, os
 
 r, c = 90, 122
-wall = np.zeros((r, c, 3))
-mask = np.zeros((r, c, 3))
-
-remaining_board = [
-	0,
-	(144 * (4 * 2)) * 7,
-	(144 * (4 * 8)) * 2,
-]
-
-
-print(remaining_board)
-assert(remaining_board[1] + remaining_board[2] >= r * c)
 
 def area(piece):
 	return piece[0] * piece[1]
@@ -36,62 +26,115 @@ def pieces():
 	random.shuffle(pieces)
 	return pieces
 
-def score(r, c, piece):
+def score(mask, remaining_board, r, c, piece):
 	a = area(piece)
-	scr = max(1, remaining_board[piece[2]]) - a
+	scr = max(1, remaining_board[piece[2]] - a)
 	other_piece = mask[r][c]
 
 	if other_piece is None:
-		return scr + 2
-
-	#if (other_piece == piece).all():
-	#	scr -= 1
+		return scr
 
 	scr += abs(area(other_piece) - a) + abs(other_piece[2] - piece[2]) * 1000
 
-	if (other_piece == rotate_piece(piece)).all():
-		scr += 1
+	if (other_piece == piece).all():
+		scr -= a
 
-	#if other_piece[2] != piece[2]:
-	#	scr += 1
+	if other_piece[2] == piece[2]:
+		scr -= a / 2
 
-	scr -= mask[r:r+piece[0], c:c+piece[1]].sum() 
+	# if this region already has pieces in it super demote
+	# piece that overlaps existing pieces
+	scr -= mask[r:r+piece[0], c:c+piece[1]].sum()
 
 	return scr #np.random.randint(0, 5)
 
-def select_piece(r, c):
+def select_piece(mask, remaining_board, r, c):
 	scores = {p:0 for p in pieces()}
 
 	for ri in range(-1, 2):
 		for ci in range(-1,2):
 			for p in pieces():
-				scores[p] += score(r, c, p)
+				scores[p] += score(mask, remaining_board, r + ri, c + ci, p)
 
 	best = list(scores.keys())[0]
-
 	for p in pieces():
 		if scores[best] < scores[p]:
 			best = p
 
-	return best
+	return scores[best], best
 
 def rotate_piece(piece):
 	return (piece[1], piece[0], piece[2])
 
-def set_piece(r, c, piece):
+def set_piece(wall, mask, remaining_board, r, c, piece):
 	wall[r:r+piece[0], c:c+piece[1]] = 0
 	wall[r+1:r+piece[0]-1, c+1:c+piece[1]-1] = [piece[2] * 64] * 3
 	mask[r:r+piece[0], c:c+piece[1]] = piece
 	remaining_board[piece[2]] -= area(piece)	
 
-for r in range(wall.shape[0]):
-	for c in range(wall.shape[1]):
-		# print(mask[r,c])
-		if mask[r,c][0] != 0 and mask[r,c][1] != 0:
-			continue
+def generate(wall, mask, remaining_board):
+	score = 0
+	placement = {}
 
-		set_piece(r, c, select_piece(r, c))
+	for r in range(wall.shape[0]):
+		for c in range(wall.shape[1]):
+			# print(mask[r,c])
+			if mask[r,c][0] != 0 and mask[r,c][1] != 0:
+				continue
 
-cv2.imwrite("walls.png", wall)
+			piece_score, piece = select_piece(mask, remaining_board, r, c)
+			set_piece(wall, mask, remaining_board, r, c, piece)
 
-print(remaining_board)
+			if piece in placement:
+				placement[piece] += 1
+			else:
+				placement[piece] = 1
+
+			score += piece_score
+
+	score -= len(placement)
+
+	return score, placement
+
+
+def generate_candidates():
+	candidates = collections.OrderedDict()
+
+	for _ in range(10):
+		wall = np.zeros((r, c, 3))
+		mask = np.zeros((r, c, 3))
+
+		remaining_board = [
+			0,
+			(144 * (4 * 2)) * 7,
+			(144 * (4 * 8)) * 2,
+		]
+
+		score, placement = generate(wall, mask, remaining_board)
+		candidates[score] = wall, placement
+
+	shutil.rmtree('walls', ignore_errors=True)
+	shutil.rmtree('placements', ignore_errors=True)
+
+	try:
+		os.mkdir('walls')
+		os.mkdir('placements')
+	except:
+		pass
+
+	i = 0
+	for score in reversed(sorted(candidates)):
+		wall, placement = candidates[score]
+
+		cv2.imwrite(f"walls/{i}.{score}.png", wall)
+
+		with open(f"placements/{i}.{score}.txt", "w+") as fp:
+			for piece in placement:
+				fp.write(f'{str(piece)}: {placement[piece]}\n')
+
+		i += 1
+
+
+
+random.seed(0)
+generate_candidates()
